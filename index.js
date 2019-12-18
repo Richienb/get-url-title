98b6f2e31dbc37fae7290f1f1dc53fa06acf3039
Richie Bendall <richiebendall@gmail.com>
2019-12-18 14:36:17 +1300

    feat: Still get title for non-200 urls
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const metascraper = require("metascraper")([
     require("metascraper-title")(),
 ])
-const ky = require("ky-universal")
+const ky = require("ky-universal").create({
+    throwHttpErrors: false,
+})
 
 module.exports = async (url) => (await metascraper({
     html: await ky(url).text(),
